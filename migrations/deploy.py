df86c69c91e533fff2726e007fc115c2e77f54b3
Tally Wiesenberg <t.j.wies@gmail.com>
2021-02-01 09:36:08 -0500

    deployment to rinkeby

--- migrations/deploy.py
+++ migrations/deploy.py
@@ -8,9 +8,9 @@ from web3.middleware import geth_poa_middleware
 w3 = Web3(Web3.HTTPProvider(config('PROVIDER')))
 print(w3.isConnected())
 
-w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+# w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-w3.eth.defaultAccount = Account.from_key(config('PRIVATE_KEY'))
+w3.eth.defaultAccount = "0x077acFebbde9fE5B055B51304B21F5b5f20BaDec"
 
 #open contract data, as compiled by Truffle
 with open('build/contracts/Lillith.json', 'r') as f:
@@ -19,16 +19,20 @@ with open('build/contracts/Lillith.json', 'r') as f:
     abi = data['abi']
 
 lillith_predeployed = w3.eth.contract(abi=abi, bytecode=bytecode)
-tx_hash = lillith_predeployed.constructor(1*10**21).buildTransaction({
-    'from': Web3.toChecksumAddress(w3.eth.defaultAccount.address),
-    'nonce': w3.eth.getTransactionCount(w3.eth.defaultAccount.address)
-})
+# tx_hash = lillith_predeployed.constructor(1*10**21).buildTransaction({
+#     'from': Web3.toChecksumAddress(w3.eth.defaultAccount.address),
+#     'nonce': w3.eth.getTransactionCount(w3.eth.defaultAccount.address)
+# })
 
-signed = w3.eth.defaultAccount.sign_transaction(tx_hash)
+# signed = w3.eth.defaultAccount.sign_transaction(tx_hash)
 
-w3.eth.sendRawTransaction(signed.rawTransaction)
+# w3.eth.sendRawTransaction(signed.rawTransaction)
+
+
+tx_hash = lillith_predeployed.constructor(1*10**21).transact()
 
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
 
 lillith_deployed = w3.eth.contract(
     address=tx_receipt.contractAddress,
